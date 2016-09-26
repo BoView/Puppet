42dfe63beb9e9c88557aa64e7581cdf20e473d8e
sunlinbo <sunlinbo@example.com>
2016-09-26 20:32:27 +0800

    The first version.

--- modules/chrony/manifests/install.pp
+++ modules/chrony/manifests/install.pp
@@ -9,13 +9,19 @@ class chrony::install{
         command=>"apt-get update",
         logoutput=>"on_failure",
         require=>File["/etc/apt/sources.list"],
+        notify=>File["/etc/apt/apt.conf"],
+    }
+    file{"/etc/apt/apt.conf":
+        ensure=>present,
+        content=>template("chrony/apt.conf.erb"),
+        require=>Exec["sources update"],
         notify=>Exec["chrony"],
     }
     exec{"chrony":
         path=>"/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/bin:/sbin",
         command=>"apt-get install chrony -y --force-yes",
         logoutput=>"on_failure",
-        require=>Exec["sources update"],
+        require=>File["/etc/apt/apt.conf"],
         notify=>Class["chrony::config"],
     }
 }
--- modules/mysql/manifests/install.pp
+++ modules/mysql/manifests/install.pp
@@ -3,19 +3,24 @@ class mysql::install{
         path=>"/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/bin:/sbin",
         logoutput=>"on_failure",
     }
-    exec{"mariadb-server":
-        command=>"apt-get install mariadb-server -y --force-yes",
+    #exec{"mariadb-server":
+    #    command=>"apt-get install mariadb-server -y --force-yes",
+    #    require=>Class["openstack_repository"],
+    #    notify=>Exec["python-pymysql"],
+    #}
+    #exec{"python-pymysql":
+    #    command=>"apt-get install python-pymysql -y --force-yes",
+    #    require=>Exec["mariadb-server"],
+    #    notify=>Exec["update_passwd"],
+    #}
+    package{
+        ["mariadb-server","python-pymysql"]:
         require=>Class["openstack_repository"],
-        notify=>Exec["python-pymysql"],
-    }
-    exec{"python-pymysql":
-        command=>"apt-get install python-pymysql -y --force-yes",
-        require=>Exec["mariadb-server"],
         notify=>Exec["update_passwd"],
     }
     exec{"update_passwd":
         command=>"mysql -uroot -e \"set password for root@localhost = password('root');\"",
-        require=>Exec["python-pymysql"],
+        require=>Package["mariadb-server","python-pymysql"],
         notify=>Exec["use_passwd"],
     }
     exec{"use_passwd":
